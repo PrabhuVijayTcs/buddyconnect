ea69663903ab1b73307d3addf198a11ff14800f7
suryasuresh04 <23243861+suryasuresh04@users.noreply.github.com>
2019-03-24 22:48:16 -0400

    tracker changes

--- UMNR-ui/src/app/main/tracker/tracker.component.ts
+++ UMNR-ui/src/app/main/tracker/tracker.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+
+declare const google: any;
+
 @Component({
   selector: 'app-tracker',
   templateUrl: './tracker.component.html',
   styleUrls: ['./tracker.component.css']
 })
 export class TrackerComponent implements OnInit {
-
-  constructor() { }
-
+ 
   ngOnInit() {
+    const trackerDetails = this.getDummyJson();
+  console.log(trackerDetails);
   }
-
+  getDummyJson() {
+    let trackerResponse = {
+      "originCode" : "SEA",
+      "destinationCode" : "JFK",
+      "destinationCity": "New York City,NY(JFK)",
+      "originCity": "Seattlle, Washington(SEA)",
+      "dateRange": "April 27, 2019",
+    };
+    return trackerResponse;
+  }
 }
