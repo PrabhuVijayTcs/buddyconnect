e486dde510db1359ff1c091231611d216b5930f8
Venkateswararao Nageswararao <venkateswararao.nageswararao@hawaiianair.com>
2020-02-18 12:43:49 -1000

    Added the dotnet project

---
+++ buddy-connect/buddy-connect/Controllers/BookingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace buddy_connect.Controllers
+{
+    public class BookingController : Controller
+    {
+        // GET: Booking
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ buddy-connect/buddy-connect/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="buddy_connect.MvcApplication" Language="C#" %>
