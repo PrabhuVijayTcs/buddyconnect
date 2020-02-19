1447825fb096f6064f8aadc5f26aab26ffd7e8ff
jijo_arulappan <jijo.arulappan@hawaiianair.com>
2020-02-18 16:21:40 -1000

    Get profile from mock data

---
+++ buddy-connect/buddy-connect/Components/LoginFunctions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using buddy_connect.Models;
+using System.Linq;
+using System.Web;
+
+namespace buddy_connect.Components
+{
+    public class LoginFunctions
+    {
+        private static List<ProfileViewModel> returnUserProfiles()
+        {
+            var sourceData = File.ReadAllText(HttpContext.Current.Server.MapPath(@"~\assets\profile.json"));
+            return JsonConvert.DeserializeObject<List<ProfileViewModel>>(sourceData);            
+        }
+
+        public static ProfileViewModel GetProfileData(string userName)
+        {
+            return returnUserProfiles().Where(x => x.UserName == userName).FirstOrDefault();            
+        }
+    }
+}
