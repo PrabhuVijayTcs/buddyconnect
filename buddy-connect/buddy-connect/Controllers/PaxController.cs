9925b2e75cfd37f6080fce0a73499a8b09783118
Prabhakaran Vijayakumar <prabhakaran.vijayakumar@hawaiianair.com>
2020-02-19 19:35:45 -1000

    pax draft

---
+++ buddy-connect/buddy-connect/Controllers/PaxController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using buddy_connect.Components;
+using buddy_connect.Models;
+
+namespace buddy_connect.Controllers
+{
+    public class PaxController : Controller
+    {
+        // GET: UserProfile
+        [HttpGet]
+        public ActionResult Info()
+        {
+            if (Session["UserProfile"] is ProfileViewModel activeUser)
+            {
+                ViewBag.Image = "/assets/img/" + activeUser.UserName + ".jpg";
+                return View(activeUser);
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
