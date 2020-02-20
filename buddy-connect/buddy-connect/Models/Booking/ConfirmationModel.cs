9c7fdb7aad478b9748a94d6192216b2518f54cc7
Venkateswararao Nageswararao <venkateswararao.nageswararao@hawaiianair.com>
2020-02-19 15:16:51 -1000

    Added confirmation page

---
+++ buddy-connect/buddy-connect/Models/Booking/ConfirmationModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace buddy_connect.Models.Booking
+{
+    public class ConfirmationModel
+    {
+        public string RecordLocator { get; set; }
+
+        public string Origin { get; set; }
+
+        public string OriginCityName { get; set; }
+
+        public string DestinationCityName { get; set; }
+
+        public string Destination { get; set; }
+
+        public DateTime DepartureDateTime { get; set; }
+
+        public DateTime ArrivalDateTime { get; set; }
+
+        public FareGroupTripAndFareDetail TripAndFareDetail { get; set; }
+    }
+}
